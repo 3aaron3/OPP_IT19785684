76b625606f108379e872abc9a1c3ded3404184bf
aaronzwe <41866109+aaronzwe@users.noreply.github.com>
2020-01-21 06:32:49 +0530

    Add files via upload
    OOP homework

---
+++ cal.java
@@ -0,0 +1,19 @@
+class cal
+{
+    public static void main(String[]args)
+    {
+        int yard=26;
+        int miles=385;
+        double kilo1;
+        double kilo2;
+        kilo1=yard/1.609;
+        kilo2=miles/1760.0;
+        System.out.println("mi-km-\t"+kilo1);
+        System.out.println("yards-km-\t"+kilo2);
+        String num="23";
+         int i=Integer.parseInt(num);
+         System.out.println(i);
+
+
+    }
+}
